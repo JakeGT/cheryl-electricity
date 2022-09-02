599f5fb446f711c866a9d789e01453fec0e0a550
JakeGT <jaketalling@rocketmail.com>
2022-09-02 19:27:10 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,14 @@
 <?php
 
+$login = fopen("login.config", "r") or die("Login details not found");
+
+$username = fgets($login);
+$password = fgets($login);
+$database_name = fgets($login);
+
 define("DB_SERVER", "localhost");
-define("DB_USERNAME", "");
-define("DB_PASSWORD", "");
-define("DB_NAME", "");
+define("DB_USERNAME", $username);
+define("DB_PASSWORD", $password);
+define("DB_NAME", $database_name);
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
